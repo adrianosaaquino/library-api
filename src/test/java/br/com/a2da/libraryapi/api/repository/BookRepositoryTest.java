4e001c8680ef2c698805f67410af07b5952fc6c8
Adriano Aquino <adrianosaaquino@gmail.com>
2020-08-18 20:10:20 -0300

    Test BookRepository

--- src/test/java/br/com/a2da/libraryapi/api/repository/BookRepositoryTest.java
+++ src/test/java/br/com/a2da/libraryapi/api/repository/BookRepositoryTest.java
@@ -11,6 +11,8 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(SpringExtension.class)
@@ -22,7 +24,7 @@ public class BookRepositoryTest {
     TestEntityManager entityManager;
 
     @Autowired
-    BookRepository repository;
+    BookRepository bookRepository;
 
     public void setUp() {
     }
@@ -36,7 +38,7 @@ public class BookRepositoryTest {
         entityManager.persist(bookSavedInstance);
 
         // When
-        boolean exists = repository.existsByIsbn(bookSavedInstance.getIsbn());
+        boolean exists = bookRepository.existsByIsbn(bookSavedInstance.getIsbn());
 
         // Then
         assertThat(exists).isTrue();
@@ -51,9 +53,54 @@ public class BookRepositoryTest {
         entityManager.persist(bookSavedInstance);
 
         // When
-        boolean exists = repository.existsByIsbn(BookHelperTest.CAPITAES_DA_AREIA_ISBN);
+        boolean exists = bookRepository.existsByIsbn(BookHelperTest.CAPITAES_DA_AREIA_ISBN);
 
         // Then
         assertThat(exists).isFalse();
+    }
+
+    @Test
+    @DisplayName("Deve obter um livro por ID")
+    public void findByIdTest() {
+
+        // Given a saved Book
+        Book bookSavedInstance = BookHelperTest.createBookWithNullId();
+        entityManager.persist(bookSavedInstance);
+
+        // When
+        Optional<Book> bookInstance = bookRepository.findById(bookSavedInstance.getId());
+
+        // Then
+        assertThat(bookInstance.isPresent()).isTrue();
+    }
+
+    @Test
+    @DisplayName("Deve salvar um livro")
+    public void saveBookTest() {
+
+        // Given a new book
+        Book bookInstance = BookHelperTest.createBookWithNullId();
+
+        // When
+        Book bookSavedInstance = bookRepository.save(bookInstance);
+
+        // Then
+        assertThat(bookSavedInstance.getId()).isNotNull();
+    }
+
+    @Test
+    @DisplayName("Deve exluir um livro")
+    public void deleteBookTest() {
+
+        // Given a saved Book
+        Book bookSavedInstance = BookHelperTest.createBookWithNullId();
+        entityManager.persist(bookSavedInstance);
+        Long bookId = bookSavedInstance.getId();
+
+        // When
+        bookRepository.delete(bookSavedInstance);
+
+        // Then
+        assertThat(bookRepository.findById(bookId)).isNotNull();
     }
 }
