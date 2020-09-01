c8279083992ea46434d45b4b949edaac93491805
Adriano Aquino <adrianosaaquino@gmail.com>
2020-09-01 13:18:59 -0300

    Removendo metodo nao utilizado

--- src/main/java/br/com/a2da/libraryapi/api/controller/book/BookDTO.java
+++ src/main/java/br/com/a2da/libraryapi/api/controller/book/BookDTO.java
@@ -1,11 +1,9 @@
 package br.com.a2da.libraryapi.api.controller.book;
 
-import br.com.a2da.libraryapi.core.model.Book;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.modelmapper.ModelMapper;
 
 import javax.validation.constraints.NotEmpty;
 
@@ -25,11 +23,4 @@ public class BookDTO {
 
     @NotEmpty
     private String isbn;
-
-    public static BookDTO bindToDTO(Book bookInstance) {
-
-        ModelMapper modelMapper = new ModelMapper();
-
-        return modelMapper.map(bookInstance, BookDTO.class);
-    }
 }
